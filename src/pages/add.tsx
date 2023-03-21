35b40d411e4673681f7031f5650e05b81aad521a
nonissue <apw@apw-mbp-2022.local>
2023-03-21 01:16:06 -0600

    fakedata.name split into fakedata.{firstName,lastName}

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -4,35 +4,42 @@ import _people from "~/demo-data.json";
 
 type Person = {
   id: number;
-  name: string;
+  firstName: string;
+  lastName: string;
 };
 
 const people = _people as Person[];
 
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="bg-slate-200">{children}</div>;
+};
+
 const AddMatch: NextPage = () => {
   return (
-    <div>
-      <div className="flex h-24 items-center justify-between p-2">
-        <div className="text-lg font-bold italic leading-tight">
-          Intermittent
-          <br />
-          Service
+    <Layout>
+      <div className="sm:mx-auto sm:max-w-4xl">
+        <div className="flex h-24 items-center justify-between px-4 py-4 shadow-md">
+          <div className="text-md font-bold italic leading-tight">
+            Intermittent
+            <br />
+            Service
+          </div>
+          <div className="">Nav Goes Here</div>
         </div>
-        <div className="">Nav Goes Here</div>
-      </div>
-      <div className="flex flex-col">
-        <div className="grid grid-cols-2 gap-4 bg-slate-100">
-          {people.map((person: Person) => (
-            <div
-              key={person.id}
-              className="flex aspect-square  items-center justify-center rounded-full border-2 border-slate-200 bg-slate-50 text-center font-mono text-xl"
-            >
-              {person.name.split(" ", 1)[0]}
-            </div>
-          ))}
+        <div className="flex flex-col">
+          <div className="grid grid-cols-2 gap-4 bg-slate-50/50 px-4 py-4 sm:grid-cols-4">
+            {people.map((person: Person) => (
+              <div
+                key={person.id}
+                className="flex h-[2.5em]  items-center justify-center rounded-md border-[1.5px] border-slate-300 bg-white text-center font-sans text-lg tracking-widest text-gray-600"
+              >
+                {person.firstName}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
