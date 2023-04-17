0fb847a25e1e1977764d673269546f26b7538afb
nonissue <andy@nonissue.org>
2023-04-16 19:25:36 -0600

    ¯\_(ツ)_/¯

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import { useState } from "react";
 import { CenterVertically } from "~/components/CenterVertically";
 
 import _people from "~/demo-data.json";
@@ -20,6 +21,25 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 };
 
 const AddMatch: NextPage = () => {
+  const [winner, setWinner] = useState<Person>();
+  const [loser, setLoser] = useState<Person>();
+
+  const toggleWinner = (person: Person) => {
+    if (winner?.id === person.id) {
+      setWinner(undefined);
+    } else {
+      setWinner(person);
+    }
+  };
+
+  const toggleLoser = (person: Person) => {
+    if (loser?.id === person.id) {
+      setLoser(undefined);
+    } else {
+      setLoser(person);
+    }
+  };
+
   return (
     <Layout>
       <CenterVertically
@@ -31,16 +51,40 @@ const AddMatch: NextPage = () => {
         }
         BelowCentered={<div>footer</div>}
       >
-        <article className="m-10 flex flex-col px-4">
-          <div className="grid grid-cols-2 gap-16 bg-slate-50/0 px-8 py-0 sm:grid-cols-4">
-            {people.map((person: Person) => (
-              <div
-                key={person.id}
-                className="flex aspect-square items-center justify-center rounded-full border-[1.5px]   border-slate-300/20 bg-white text-center font-sans text-2xl font-bold  text-gray-600"
-              >
-                {person.firstName}
-              </div>
-            ))}
+        <article className=" mx-auto flex w-full max-w-4xl flex-row flex-wrap">
+          <div className="w-1/2 ">
+            <h3 className="my-4 text-xl font-bold">Winner</h3>
+            <div className="grid grid-cols-2 gap-1 p-2 sm:grid-cols-2">
+              {people.map((person: Person) => (
+                <button
+                  key={person.id}
+                  onClick={() => toggleWinner(person)}
+                  className="flex h-[3em] items-center justify-center rounded border-[1.5px] border-slate-300 bg-white text-center font-sans text-sm font-normal uppercase  text-gray-600 disabled:opacity-30"
+                  disabled={loser?.id === person.id ? true : false}
+                >
+                  {person.firstName}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className="w-1/2 ">
+            <h3 className="my-4 text-xl font-bold">Loser</h3>
+            <div className="grid grid-cols-2 gap-1 p-2 sm:grid-cols-2">
+              {people.map((person: Person) => (
+                <button
+                  key={person.id}
+                  onClick={() => toggleLoser(person)}
+                  className="flex h-[3em] items-center justify-center rounded border-[1.5px] border-slate-300 bg-white text-center font-sans text-sm font-normal uppercase text-gray-600  disabled:opacity-30"
+                  disabled={winner?.id === person.id ? true : false}
+                >
+                  {person.firstName}
+                </button>
+              ))}
+            </div>
+          </div>
+          <div className="my-4 flex flex-col text-left text-lg">
+            <p className="">Winner: {winner?.firstName}</p>
+            <p className="">Loser: {loser?.firstName}</p>
           </div>
         </article>
       </CenterVertically>
