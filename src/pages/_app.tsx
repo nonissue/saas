cd90e8423a916cd21bd93d17e250cbfcd1ac608d
nonissue <andy@nonissue.org>
2023-05-05 14:11:05 -0600

    add next-themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
+import Inspect from "inspx";
+import { ThemeProvider } from "next-themes";
+
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "~/styles/globals.css";
 import "~/styles/center-vertically.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Inspect>
+      <ThemeProvider attribute="class" defaultTheme="system">
+        <Component {...pageProps} />{" "}
+      </ThemeProvider>
+    </Inspect>
+  );
 };
 
 export default MyApp;
