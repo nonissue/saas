5327557bbd8ca2128554db8275fc57ecb7bc44cb
nonissue <andy@nonissue.org>
2023-05-31 21:16:56 -0600

    add fake player and game data for dev.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { type NextPage } from "next";
+import { Header } from "~/components";
 import { CenterVertically } from "~/components/CenterVertically";
 import { MatchScoreboard } from "~/components/MatchScoreboard";
 
@@ -11,22 +12,57 @@ interface Person {
   lastName: string;
 }
 
+const fakePlayerAndy = {
+  firstName: "Andy",
+  lastName: "Murray",
+  nickName: "Muzza",
+  id: 1,
+};
+
+const fakePlayerKellan = {
+  firstName: "Kellan",
+  lastName: "Easterday",
+  nickName: "KEYLIME",
+  id: 2,
+};
+
+const fakeGame1 = {
+  id: 1,
+  winner: fakePlayerAndy,
+  loser: fakePlayerKellan,
+  winnerScore: 11,
+  loserScore: 9,
+};
+
+const fakeGame2 = {
+  id: 2,
+  winner: fakePlayerKellan,
+  loser: fakePlayerAndy,
+  winnerScore: 13,
+  loserScore: 11,
+};
+
+const fakeGame3 = {
+  id: 3,
+  winner: fakePlayerKellan,
+  loser: fakePlayerAndy,
+  winnerScore: 11,
+  loserScore: 1,
+};
+
 const AddMatch: NextPage = () => {
   return (
     <Layout>
       <CenterVertically
-        AboveCentered={
-          <div className="flex items-center justify-between bg-slate-800 px-4 py-4 shadow-md">
-            <div className="text-md font-bold leading-tight">SaaS</div>
-            <div className="">Nav</div>
-          </div>
-        }
+        AboveCentered={<Header />}
         BelowCentered={<div>footer</div>}
       >
         <article className="mx-auto flex w-full max-w-4xl flex-row flex-wrap ">
-          <h1>SaaS!!</h1>
-          <section>
-            {/* <MatchScoreboard /> */}
+          <section className="w-full border-2 ">
+            <MatchScoreboard
+              players={[fakePlayerAndy, fakePlayerKellan]}
+              games={[fakeGame1, fakeGame2, fakeGame3]}
+            />
             {/* <MatchScoreboard /> */}
           </section>
         </article>
