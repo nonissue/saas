64a21c5f6a2359dfa602c79d6e2e1931e288f7cb
nonissue <andy@nonissue.org>
2023-05-31 21:16:06 -0600

    add header components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,18 @@ import { HeaderLink, ThemeChanger } from "~/components";
 
 const Header = () => {
   return (
-    <div className="relative z-10 shadow-sm dark:border-b dark:border-gray-900">
+    <div className="relative z-10  shadow-sm dark:border-b dark:border-gray-900">
       <div className="relative inset-0 z-20">
         <div className="inset-0 mx-auto flex max-w-3xl px-6 py-2 sm:px-6 lg:px-8 lg:py-2 ">
           <div className="space-between relative flex w-full items-center">
             <div className="font-display flex-grow ">
-              <Link href="/">
-                <a className=" flex items-center">
-                  <div className="text-lg font-bold tracking-tight text-gray-900 dark:text-gray-50 sm:text-xl md:text-2xl">
-                    andy
-                  </div>
-                  <div className="font-display text-xs tracking-tight text-gray-900 text-opacity-80  first-letter:text-opacity-100 dark:text-gray-400 sm:text-base">
-                    .ws
-                  </div>
-                </a>
+              <Link href="/" className=" flex items-center">
+                <div className="text-lg font-bold tracking-tight text-slate-900 dark:text-gray-50 sm:text-xl md:text-2xl">
+                  GoodService
+                </div>
+                <div className="font-mono text-xs font-bold  text-slate-400 first-letter:text-opacity-100 sm:text-xs">
+                  TM
+                </div>
               </Link>
             </div>
             <HeaderLink href="/projects" title="Projects" />
--- src/components/Header/HeaderLink.tsx
+++ src/components/Header/HeaderLink.tsx
@@ -5,19 +5,18 @@ interface HeaderLinkProps {
   title: string;
 }
 
-const NewHeaderLink = ({ title, href }: HeaderLinkProps) => {
+const HeaderLink = ({ title, href }: HeaderLinkProps) => {
   return (
     <div>
-      <Link href={href}>
-        <a
-          className="font-sans text-xs font-medium capitalize tracking-wide opacity-90 transition-colors duration-200 hover:text-blue-600
-        dark:hover:text-blue-400 sm:text-base "
-        >
-          {title}
-        </a>
+      <Link
+        href={href}
+        className="font-sans text-xs font-medium capitalize tracking-wide opacity-90 transition-colors duration-200 hover:text-blue-600
+      dark:hover:text-blue-400 sm:text-base "
+      >
+        {title}
       </Link>
     </div>
   );
 };
 
-export { NewHeaderLink };
+export { HeaderLink };
