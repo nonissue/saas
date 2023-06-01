66635559321af1fc65ba8255ae47fdd063eb2581
nonissue <andy@nonissue.org>
2023-05-31 21:16:46 -0600

    Rough pass at MatchScoreboard component.

--- src/components/MatchScoreboard.tsx
+++ src/components/MatchScoreboard.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+// ^ allows this: player={players[0]!} which im using during dev with mock data as
+// typescript always sees the items in an array of objects as possibly undefined.
+
 // see Figma -> Squashed -> MatchScoreboard_1
 
 interface Player {
@@ -30,16 +34,52 @@ interface MatchScoreboardRowProps {
   games: Game[];
 }
 
+/*
+
+Rough sketch of scoreboard:
+
+┌─────────────────┬─────────┬─────────┬─────────┐
+│ Player 1 (name) │ Game 1  │ Game 2  │ Game 3  │
+├─────────────────┼─────────┼─────────┼─────────┤
+│ Player 2 (name) │ Game 1  │ Game 2  │ Game 3  │
+└─────────────────┴─────────┴─────────┴─────────┘
+
+
+
+*/
+
 const MatchScoreBoardRow = ({ player, games }: MatchScoreboardRowProps) => {
   return (
-    <div>
-      <div>
-        {player.firstName.slice(1)} {player.lastName}
+    <div
+      aria-label="Player"
+      className="flex max-w-xl flex-row items-center justify-between"
+    >
+      {/* Name */}
+      <div className=" font-sans text-3xl font-semibold tracking-tight">
+        {player.firstName}
       </div>
-      <div>
-        {games.map((game) => (
-          <div key={game.id}>{game.id}</div>
-        ))}
+      {/* Loop over all game scores and display them */}
+      <div
+        aria-label="Player Game Scores"
+        className="flex flex-row gap-x-2 align-middle font-mono font-semibold"
+      >
+        {games.map((game) =>
+          game.winner.id === player.id ? (
+            <div
+              key={game.id}
+              className="w-[4ch] border-2 border-sky-500/20 bg-sky-300 p-2"
+            >
+              {game.winnerScore}
+            </div>
+          ) : (
+            <div
+              key={game.id}
+              className="w-[4ch] border-2 border-slate-300/20 bg-slate-100 p-2"
+            >
+              {game.loserScore}
+            </div>
+          )
+        )}
       </div>
     </div>
   );
@@ -47,14 +87,10 @@ const MatchScoreBoardRow = ({ player, games }: MatchScoreboardRowProps) => {
 
 const MatchScoreboard = ({ players, games }: MatchScoreboardProps) => {
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-row">
-        <div>
-          <h1>Scores</h1>
-          <div>Score</div>
-        </div>
-
-        <div>Player Name</div>
+    <div className="flex flex-col p-4">
+      <div className="flex flex-col gap-y-2">
+        <MatchScoreBoardRow player={players[0]!} games={games} />
+        <MatchScoreBoardRow player={players[1]!} games={games} />
       </div>
     </div>
   );
